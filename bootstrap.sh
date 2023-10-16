a4b177cf522a81cdb7a8775131c9b5b2deb3071c
Emily Kauffman <kauffmanes@users.noreply.github.com>
2023-10-16 09:26:32 -0600

    Update bootstrap.sh
    added an nvm autoload

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,25 @@ git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.br "branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset
 ) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate"
+
+# place this after nvm initialization!
+autoload -U add-zsh-hook
+load-nvmrc() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
